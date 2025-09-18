145f43065a6b3e0d0148eadbd70f89cabcf3b740
gjellin <146391953+gjellin@users.noreply.github.com>
2025-09-18 15:07:32 -0700

    Add files via upload

--- vo_iframe_bug3/script.js
+++ vo_iframe_bug3/script.js
@@ -1,19 +1,18 @@
 document.getElementById('submitButton').addEventListener('click', function() {
-    alert('test');
-
-const liveRegion = document.getElementById('liveRegion');
-const randomWords = generateRandomWords(3);
-liveRegion.textContent = randomWords.join(' ');
+  const liveRegion = document.getElementById('liveRegion');
+  const randomWords = generateRandomWords(3);
+  liveRegion.innerHTML = `<pre tabindex="-1">${randomWords.join(' ')}</pre>`;
 });
+
 function generateRandomWords(num) {
-const words = [
-'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon',
-'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'vanilla'
-];
-let randomWords = [];
-for (let i = 0; i < num; i++) {
-const randomIndex = Math.floor(Math.random() * words.length);
-randomWords.push(words[randomIndex]);
-}
-return randomWords;
+  const words = [
+    'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon',
+    'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'vanilla'
+  ];
+  let randomWords = [];
+  for (let i = 0; i < num; i++) {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    randomWords.push(words[randomIndex]);
+  }
+  return randomWords;
 }
